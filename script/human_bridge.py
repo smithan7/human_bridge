510cca7ccfd0d48b56658a03f2dfceaf008e079b
Andy <sgt.a.smith@gmail.com>
2017-09-14 08:37:18 -0700

    prep for hardware

--- script/human_bridge.py
+++ script/human_bridge.py
@@ -9,6 +9,8 @@ if __name__ == "__main__":
 
 	rospy.init_node("Human_Bridge")
 	environment_number = rospy.get_param('~test_environment_number') 
+	nw_corner = []
+	se_corner = []
 
 	# hardware test environment
 	if environment_number == 3:
@@ -24,6 +26,15 @@ if __name__ == "__main__":
 	if environment_number == 4:
 		nw_corner = [44.539457, -123.250866]
 		se_corner = [44.538162, -123.247776]
+
+		# bigger test environment
+	if environment_number == 5:
+		nw_corner = [44.539457, -123.250866]
+		se_corner = [44.538162, -123.247776]
+
+	if environment_number == 5:	
+		nw_corner = [44.539580, -123.250876]
+		se_corner = [44.538725, -123.247601]
 
 	h = human( nw_corner, se_corner )	
 	rospy.spin()
